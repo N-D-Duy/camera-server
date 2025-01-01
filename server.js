e2dc481e7276f0e8ba68b760e3d0805c25f8d49a
namng <nllsover8.0@gmail.com>
2025-01-01 14:16:21 +0700

    fix: http port

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const WS_PORT  = 8888;
 const HTTP_PORT = 8000;
 
-// Add CORS middleware
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
+// // Add CORS middleware
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+// });
 
 const wsServer = new WebSocket.Server({port: WS_PORT}, ()=> console.log(`WS Server is listening at ${WS_PORT}`));
 
